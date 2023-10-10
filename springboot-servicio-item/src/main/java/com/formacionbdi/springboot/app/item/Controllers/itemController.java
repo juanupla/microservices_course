9199629bec85fa765c4d1001a50a11f069d1bc6b
113932_Cremona_Juan <juan_crub@outlook.com>
2023-10-10 20:55:01 -0300

    finished - CircuitBreaker

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/Controllers/itemController.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/Controllers/itemController.java
@@ -3,6 +3,8 @@ package com.formacionbdi.springboot.app.item.Controllers;
 import com.formacionbdi.springboot.app.item.Models.Item;
 import com.formacionbdi.springboot.app.item.Models.Producto;
 import com.formacionbdi.springboot.app.item.Services.IItemService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
@@ -10,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 //@RequestMapping("/Items")
@@ -35,12 +38,37 @@ public class itemController {
         System.out.println(token);    //a los request solo los mostraremos en consola
         return ResponseEntity.ok(iItemService.finAll());
     }
+
     //-----------------------------------------------------------------------------------------------------------------------
-    //                  CONTIENE EJEMPLO DE CIRCUITBREAKER FACTORY: en el cual podemos usar los parametros por defecto o configurarlos nosotros (Bean en la clase RestTemplateConfig )
+    //                  CONTIENE EJEMPLO DE CIRCUITBREAKER FACTORY: en el cual podemos usar los parametros por defecto o
+    //                  configurarlos nosotros (Bean en la clase RestTemplateConfig )
     @GetMapping("/ItemById/{id}/Cantidad/{cantidad}")
     public ResponseEntity<Item> itemById(@PathVariable Long id,@PathVariable Integer cantidad){
         return circuitBreakerFactory.create("items")//Este es el nombre del cortocircuito y luego el .run() donde usamos una expresion lambda
-                .run(()->ResponseEntity.ok(iItemService.finById(id,cantidad)), e -> metodoAlternativo(id,cantidad)); //en caso de error "e" llamara al metodo alternativo; de no tenerlo, cada vez que abra el cortocircuito o se envie una solicitud insatisfactoria devolvera un error "NoFallBackAvailableException"
+                .run(()->ResponseEntity.ok(iItemService.finById(id,cantidad)), e -> metodoAlternativo(id,cantidad)); //en caso de error "e" llamara al metodo alternativo;
+        // de no tenerlo, cada vez que abra el cortocircuito o se envie una solicitud insatisfactoria devolvera un error "NoFallBackAvailableException"
+    }
+
+
+    //-----------------------------------------------------------------------------------------------------------------------
+    //                  CONTIENE EJEMPLO DE CIRCUITBREAKER utilizando anotaciones.
+    //                      esto es pruedente hacerlo en el servicio, aca hacemos foco en el funcionamiento
+    //                      SIEMPRE que utilizamos anotaciones las configuraciones deben estar si o si en el yml y no puede
+    //                      ser implementado con un @Bean
+    @CircuitBreaker(name="items", fallbackMethod = "metodoAlternativo")
+    @GetMapping("/ItemById2/{id}/Cantidad/{cantidad}")
+    public ResponseEntity<Item> itemById2(@PathVariable Long id,@PathVariable Integer cantidad){
+        return ResponseEntity.ok(iItemService.finById(id,cantidad));
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------
+    //                  CONTIENE EJEMPLO DE CIRCUITBREAKER utilizando anotaciones.
+    //                      acá implementamos timeout con anotaciones, usamos el @TimeLimiter!
+    @TimeLimiter(name = "items", fallbackMethod = "metodoAlternativo2")
+    @GetMapping("/ItemById3/{id}/Cantidad/{cantidad}")
+    public CompletableFuture<ResponseEntity<Item>> itemById3(@PathVariable Long id, @PathVariable Integer cantidad){
+        return CompletableFuture.supplyAsync(()->
+                ResponseEntity.ok(iItemService.finById(id,cantidad))); //CONSUMO ASINCRONICO!!
     }
 
     public ResponseEntity<Item> metodoAlternativo(Long id, Integer cantidad){
@@ -52,5 +80,15 @@ public class itemController {
         producto.setPrecio(500.00);
         item.setProducto(producto);
         return ResponseEntity.ok(item);
+    }
+    public CompletableFuture<ResponseEntity<Item>> metodoAlternativo2(Long id, Integer cantidad){
+        Item item = new Item();
+        Producto producto = new Producto();
+        item.setCantidad(cantidad);
+        producto.setId(id);
+        producto.setNombre("Camara Sony");
+        producto.setPrecio(500.00);
+        item.setProducto(producto);
+        return CompletableFuture.supplyAsync(()->ResponseEntity.ok(item));
     }
 }
